f31187e544ec9b6df658ae5e9066fcd30a75e997
Mandla <mandla.magagula@gmail.com>
2020-09-09 16:37:31 +0200

    add bootstrap solution and add simple README.md

---
+++ OzowSorter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OzowSorter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
