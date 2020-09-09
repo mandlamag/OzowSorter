eb76aa5622a30d1d1ea318081673de0fbbca5039
Mandla <mandla.magagula@gmail.com>
2020-09-09 16:53:40 +0200

    Passing Test: Given A string should remove blank spaces and panctuation marks

--- OzowSorter.Tests/SorterUnitTests.cs
+++ OzowSorter.Tests/SorterUnitTests.cs
@@ -3,12 +3,20 @@ using Xunit;
 
 namespace OzowSorter.Tests
 {
-    public class UnitTest1
+    public class SorterUnitTests
     {
         [Fact]
-        public void Test1()
+        public void GivenAnArbitraryString_ShouldRemoveEmptySpaces()
         {
+            var input = "Contrary to popular belief the pink unicorn flies east";
 
+            var expected = "Contrarytopopularbeliefthepinkunicornflieseast";
+
+            var actual = input.RemoveWhiteSpacesAndPunctuation();
+
+
+            Assert.Equal(expected, actual);
         }
+
     }
 }
--- OzowSorter/StringSorter.cs
+++ OzowSorter/StringSorter.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+public static class StringSorter
+{
+    public static string RemoveWhiteSpacesAndPunctuation(this string str)
+    {
+        return Regex.Replace(str, @"[^\w]", String.Empty);
+    }
+
+}
